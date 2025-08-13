d01d25d84eda410f02a046db0815f95c9ee9d5ea
landaudiogo <diogo.hewitt.landau@hotmail.com>
2025-08-13 16:24:35 +0200

    Use secret config path to point to identity file

--- hosts/alcaraz/home-manager/ssh.nix
+++ hosts/alcaraz/home-manager/ssh.nix
@@ -1,10 +1,5 @@
-{ pkgs, config, ... }: 
+{ pkgs, config, nixosConfig, ... }: 
 {
-    # age.secrets.landaudiogo-ed25519 = {
-    #     file = ../../../secrets/landaudiogo-ed25519.age;
-    #     path = "${config.xdg.configHome}/.ssh/id_ed25519";
-    # };
-    # home.file."${config.xdg.configHome}/.ssh/id_ed25519.pub" = ;
     programs.ssh = {
         enable = true;
         matchBlocks = {
@@ -12,37 +7,41 @@
                 user = "landaudiogo";
                 hostname = "10.0.0.1";
                 port = 22;
-                identityFile = "~/.ssh/id_ed25519";
+                identityFile = nixosConfig.age.secrets.landaudiogo-ed25519.path;
+                identitiesOnly = true;
             };
             sinner = {
                 user = "landaudiogo";
                 hostname = "10.0.0.2";
                 port = 22;
-                identityFile = "~/.ssh/id_ed25519";
+                identityFile = nixosConfig.age.secrets.landaudiogo-ed25519.path;
+                identitiesOnly = true;
             };
             gijs1 = {
                 user = "ubuntu";
                 hostname = "18.197.157.103";
                 port = 22;
-                identityFile = "~/.ssh/id_ed25519";
+                identityFile = "~/.ssh/gijs.pem";
             };
             gijs2 = {
                 user = "ubuntu";
                 hostname = "3.65.2.55";
                 port = 22;
-                identityFile = "~/.ssh/id_ed25519";
+                identityFile = "~/.ssh/gijs.pem";
             };
             gijs3 = {
                 user = "ubuntu";
                 hostname = "18.193.68.182";
                 port = 22;
-                identityFile = "~/.ssh/id_ed25519";
+                identityFile = nixosConfig.age.secrets.landaudiogo-ed25519.path;
+                identitiesOnly = true;
             };
             joris = {
                 user = "ubuntu";
                 hostname = "3.68.216.145";
                 port = 22;
-                identityFile = "~/.ssh/id_ed25519";
+                identityFile = nixosConfig.age.secrets.landaudiogo-ed25519.path;
+                identitiesOnly = true;
             };
         };
     };
